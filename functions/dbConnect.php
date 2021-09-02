7805946ac27e104b2abf9293e4c1688954aeb86f
akihiro-f <akihirobluesharks@gmail.com>
2021-09-03 01:38:05 +0900

    dbc 追加

--- functions/dbConnect.php
+++ functions/dbConnect.php
@@ -1,7 +1,7 @@
 <?php
-require_once '../env.php';
+require_once dirname(__FILE__) . '/../env.php';
 ini_set('display_errors', true);
-function connect()
+function dbc()
 {
     $host = DB_HOST;
     $user = DB_USER;
@@ -15,6 +15,7 @@ function connect()
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
       ]);
+        echo 'success';
         return $pdo;
     } catch (PDOException $e) {
         echo'接続失敗です！'.$e->getMessage();
