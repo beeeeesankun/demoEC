08539ae108bbcbb2d6dc2b714d0ed114ba38bf9d
akihiro-f <akihirobluesharks@gmail.com>
2021-09-12 22:54:11 +0900

    ダッシュボード追加

--- view/component/header.php
+++ view/component/header.php
@@ -1,7 +1,10 @@
 <header class="header">
   <h2>マイページ</h2>
-  <div>
+  <div class="flex">
     <p>ようこそ<?php echo h($login_user['name'])?>さん</p>
+    <form action="logout.php" method="POST" class="logout">
+      <input type="submit" name="logout" value="ログアウト" class="button">
+  </form>
   </div>
 </header>
 <style>
@@ -12,5 +15,30 @@
     background-color: #ff7052;
     color: #fff;
     padding: 0 20px;
+  }
+  .flex{
+    display: flex;
+    align-items: center;
+  }
+  .logout{
+    margin: 0 0 0 20px;
+  }
+  .button {
+    display: inline-block;
+    border-radius: 5%;
+    font-size: 10pt;
+    text-align: center;
+    cursor: pointer;
+    padding: 10px 20px;
+    background: rgba(255, 112, 82, 0.8);
+    color: #ffffff;
+    line-height: 1em;
+    transition: .3s;
+    box-shadow: 3px 3px 3px #666666;
+  }
+
+  .button:hover {
+    box-shadow: none;
+    opacity: .75;
   }
 </style>
--- view/mypage.php
+++ view/mypage.php
@@ -19,22 +19,40 @@ $el = '<div id="alert" class="alert"><p>ログインしました</p></div>';
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="<?php echo CSS.'mypage.css'?>">
   <title>マイページ</title>
 </head>
 
 <body>
-  <?php echo $el; ?>
-  <?php include './component/header.php'; ?>
   <div class="wrapper">
-    <?php include './component/side.php'; ?>
-    <?php include './component/main.php'; ?>
+    <?php echo $el; ?>
+    <?php include './component/header.php'; ?>
+    <section class="dashboard">
+      <h4>ダッシュボード</h4>
+      <ul class="lists">
+        <?php if ($login_user['id'] == 1 && $login_user['name'] == 'master') :?>
+        <li class="lists-item">
+          <div>
+            <i class="fas fa-chalkboard-teacher"></i>
+          </div>
+          <p>アカウント管理</p>
+        </li>
+        <?php endif; ?>
+        <li class="lists-item">
+          <div>
+            <i class="fas fa-boxes"></i>
+          </div>
+          <p>商品管理</p>
+        </li>
+      </ul>
+    </section>
   </div>
   <script src="<?php echo JS.'mypage.js'?>"></script>
 </body>
